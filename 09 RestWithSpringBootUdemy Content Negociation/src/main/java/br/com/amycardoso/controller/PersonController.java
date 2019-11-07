8cd06d30a764ecd8b1edd9c39578b3f0e36415e1
amycardoso <alice.cardoso10@gmail.com>
2019-11-07 00:19:08 -0500

    Content Negociation

---
+++ 09 RestWithSpringBootUdemy Content Negociation/src/main/java/br/com/amycardoso/controller/PersonController.java
@@ -0,0 +1,54 @@
+package br.com.amycardoso.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.amycardoso.data.vo.v1.PersonVO;
+import br.com.amycardoso.services.PersonServices;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}	
+	
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO findById(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}	
+	
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO update(@RequestBody PersonVO person) {
+		return service.update(person);
+	}	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}	
+	
+}
