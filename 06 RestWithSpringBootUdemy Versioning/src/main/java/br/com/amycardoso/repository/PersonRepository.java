f48cbeea4e2e9a931056870d431baa85611ad577
amycardoso <alice.cardoso10@gmail.com>
2019-11-07 00:06:18 -0500

    Versioning

---
+++ 06 RestWithSpringBootUdemy Versioning/src/main/java/br/com/amycardoso/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package br.com.amycardoso.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.amycardoso.data.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
