ce61b26c22f8e6b80141fb5cf087dad6a8e7e9dd
amycardoso <alice.cardoso10@gmail.com>
2019-11-06 23:26:57 -0500

    Eclipse Scaffold

---
+++ 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/amycardoso/restwithspringbootudemy/RestWithSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.amycardoso.restwithspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootUdemyApplication.class, args);
+	}
+
+}
