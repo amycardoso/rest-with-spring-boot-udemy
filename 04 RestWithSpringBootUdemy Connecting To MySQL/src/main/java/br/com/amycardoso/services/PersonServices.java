249e3453f72106cacd8db6d1865656807ceba66f
amycardoso <alice.cardoso10@gmail.com>
2019-11-06 23:54:26 -0500

    Connecting To MySQL

---
+++ 04 RestWithSpringBootUdemy Connecting To MySQL/src/main/java/br/com/amycardoso/services/PersonServices.java
@@ -0,0 +1,50 @@
+package br.com.amycardoso.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.amycardoso.exception.ResourceNotFoundException;
+import br.com.amycardoso.model.Person;
+import br.com.amycardoso.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	@Autowired
+	PersonRepository repository;
+		
+	public Person create(Person person) {
+		return repository.save(person);
+	}
+	
+	public List<Person> findAll() {
+		return repository.findAll();
+	}	
+	
+	public Person findById(Long id) {
+
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+	}
+		
+	public Person update(Person person) {
+		Person entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(entity);
+	}	
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+	}
+
+}
