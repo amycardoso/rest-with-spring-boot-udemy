3220f7dad4f4602587cdac766e6be95cc806483e
amycardoso <alice.cardoso10@gmail.com>
2019-11-09 00:05:45 -0500

    Continuous Integration with Travis CI, Docker Hub, Amazon ECR and ECS

---
+++ 19 RestWithSpringBootUdemy CI na AWS com o Travis CI/RestWithSpringBootUdemy/src/main/java/br/com/amycardoso/config/FileStorageConfig.java
@@ -0,0 +1,17 @@
+package br.com.amycardoso.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix= "file")
+public class FileStorageConfig {
+
+	private String uploadDir;
+
+	public String getUploadDir() {
+		return uploadDir;
+	}
+
+	public void setUploadDir(String uploadDir) {
+		this.uploadDir = uploadDir;
+	}
+}
